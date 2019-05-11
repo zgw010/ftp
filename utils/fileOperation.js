bbb5a3d43bae169430c948cc977c818fc1ac4773
ZGW <lleaff@qq.com>
2019-05-11 20:07:51 +0800

    :sparkles: add delete directory function

--- utils/fileOperation.js
+++ utils/fileOperation.js
@@ -1,29 +1,47 @@
 const fs = require('fs')
-function readDirSync(path, fileTreeObject = {}) {
-  var pathList = fs.readdirSync(path);
+function readDirSync(dir, fileTreeObject = {}) {
+  var pathList = fs.readdirSync(dir);
   pathList.forEach(function (fileName) {
     fileTreeObject[fileName] = {};
-    var info = fs.statSync(path + '/' + fileName)
+    var info = fs.statSync(dir + '/' + fileName)
     if (info.isDirectory()) {
       fileTreeObject[fileName].htmlType = 'ul';
       fileTreeObject[fileName].children = {};
-      readDirSync(path + '/' + fileName, fileTreeObject[fileName].children);
+      readDirSync(dir + '/' + fileName, fileTreeObject[fileName].children);
     } else {
       fileTreeObject[fileName].htmlType = 'li';
     }
   })
   return fileTreeObject;
 }
-function deleteFileSync(file){
+function deleteFileSync(dir) {
   try {
-    fs.unlinkSync(file);
-    console.log(`已成功删除 ${file} `);
+    fs.unlinkSync(dir);
+    console.log(`已成功删除 ${dir} `);
   } catch (err) {
     // 处理错误
-    console.log(`删除文件 [${file}] 出错`);
+    console.log(`删除文件 [${dir}] 出错`);
   }
 }
+
+function deleteDirectorySync(dir) {
+  const files = fs.readdirSync(dir);
+  const newPath = `${dir}/${files[i]}`;
+  for (let i = 0; i < files.length; i++) {
+    const info = fs.statSync(newPath)
+    if (info.isDirectory()) {
+      //如果是文件夹就递归下去
+      deleteDirectorySync(newPath);
+    } else {
+      //删除文件
+      fs.unlinkSync(newPath);
+    }
+  }
+  fs.rmdirSync(dir)//如果文件夹是空的，就将自己删除掉
+}
+
 module.exports = {
   readDirSync,
-  deleteFileSync
+  deleteFileSync,
+  deleteDirectorySync
 }
